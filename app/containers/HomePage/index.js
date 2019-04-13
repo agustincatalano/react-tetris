d9ae734c2f60935a5a42c1df02616845f118e056
agustin <agustincatalano@gmail.com>
2019-04-13 22:27:02 +0200

    define components and blocks

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -11,10 +11,17 @@
 
 import React from 'react';
 import styled from 'styled-components';
+import tetrisImage from '../../images/tetris.jpg';
 
 const GameBlock = styled.div`
   flex: 1;
   border: 1px solid red;
+  class: game-block;
+  background-image: url(${tetrisImage});
+  display: flex;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
 `;
 
 const InfoBlock = styled.div`
@@ -27,13 +34,48 @@ const MainPage = styled.div`
   border: 1px solid green;
 `;
 
+const ScoreComponent = styled.li`
+  border: 1px solid green;
+`;
+
+const LineComponent = styled.li`
+  border: 1px solid green;
+`;
+
+const NextComponent = styled.li`
+  border: 1px solid green;
+`;
+
+const ArrowComponent = styled.li`
+  border: 1px solid green;
+`;
+
+const StartComponent = styled.button`
+  border: 1px solid green;
+`;
+
 /* eslint-disable react/prefer-stateless-function */
 export default class HomePage extends React.PureComponent {
   render() {
     return (
-      <MainPage>
-        <GameBlock>this is the game container1111</GameBlock>
-        <InfoBlock>this is the info container2222</InfoBlock>
+      <MainPage className="main-page">
+        <GameBlock background="app/images/tetris.jpg" className="game-Block">
+          this is the game container1111
+        </GameBlock>
+        <InfoBlock className="info-Block">
+          this is the info container2222
+          <ScoreComponent className="score-component">
+            {' '}
+            Score: xxx
+          </ScoreComponent>
+          <LineComponent className="line-component"> Line: xx</LineComponent>
+          <NextComponent className="next-component"> next: </NextComponent>
+          <ArrowComponent className="arrows-component">
+            {' '}
+            arrows here{' '}
+          </ArrowComponent>
+          <StartComponent className="start-component"> Start </StartComponent>
+        </InfoBlock>
       </MainPage>
     );
   }
