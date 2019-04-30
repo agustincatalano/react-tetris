ebd972278b7e1615bfd7ac9be8c86e3a9810d3fb
acatalano <agustin.catalano@williamhill.com>
2019-04-30 16:18:43 +0200

    Refactoring

--- app/components/Info/InfoBlock.js
+++ app/components/Info/InfoBlock.js
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
+import React from 'react';
 import infoImage from '../../images/info-image.jpg';
-import { StartGame } from './StartGame';
+import Score from './Score';
+import LineCounter from './LineCounter';
+import NextTetromino from './NextTetromino';
+import Arrows from './Arrows';
 
 export const MyInfoBlock = styled.div`
   width: 200px;
@@ -8,10 +12,18 @@ export const MyInfoBlock = styled.div`
   background-image: url(${infoImage});
   background-size: cover;
 `;
+
 // use StartGame in here somehow
 
-export class InfoBlock extends React.Component {
-  render() {
-    return MyInfoBlock;
-  }
+export function InfoBlock() {
+  return (
+    <MyInfoBlock>
+      {'this is the info container'}
+
+      <Score>this is the Score Component</Score>
+      <LineCounter>this is the LineScore Component</LineCounter>
+      <NextTetromino>this is the NextTetromino Component</NextTetromino>
+      <Arrows>this is the Arrows Component</Arrows>
+    </MyInfoBlock>
+  );
 }
--- app/containers/HomePage/HomePage.js
+++ app/containers/HomePage/HomePage.js
@@ -7,26 +7,11 @@ const MainPage = styled.div`
   border: 1px solid green;
 `;
 
-export class HomePage extends React.PureComponent {
-  render() {
-    return (
-      <MainPage className="main-page">
-        <GameBlock background="app/images/tetris.jpg" className="game-Block">
-          {'this is the game container1111 '}
-        </GameBlock>
-        <InfoBlock className="info-Block">
-          {'this is the info container2222'}
-          <ScoreComponent className="score-component">
-            {'Score: xxx '}
-          </ScoreComponent>
-          <LineComponent className="line-component">Line: xx</LineComponent>
-          <NextComponent className="next-component"> next: </NextComponent>
-          <ArrowComponent className="arrows-component">
-            {'arrows here'}
-          </ArrowComponent>
-          <StartComponent className="start-component">Start</StartComponent>
-        </InfoBlock>
-      </MainPage>
-    );
-  }
+export function HomePage() {
+  return (
+    <MainPage>
+      <GameBlock />
+      <InfoBlock />
+    </MainPage>
+  );
 }
