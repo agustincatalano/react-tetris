bd22424208ca7c30dfcad0593798ef63b9fb4c98
acatalano <agustin.catalano@williamhill.com>
2019-05-17 16:04:46 +0200

    added new tetrominos

--- app/components/figures/I.js
+++ app/components/figures/I.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Square} from './Square'
+import { Square } from './Square';
 
 export class I extends React.Component {
   render() {
@@ -8,13 +8,26 @@ export class I extends React.Component {
     const y = 20;
     const border = 1;
 
-    return (
-      <>
-        <Square x={x} y={y} side={side} />
-        <Square x={x} y={y + side + border} side={side} />
-        <Square x={x} y={y + 2 * (side + border)} side={side} />
-        <Square x={x} y={y + 3 * (side + border)} side={side} />
-      </>
-    );
+    const { horizontal } = this.props;
+
+    if (horizontal) {
+      return (
+        <>
+          <Square x={x} y={y} side={side} />
+          <Square x={x + side + border} y={y} side={side} />
+          <Square x={x + (side + border) * 2} y={y} side={side} />
+          <Square x={x + (side + border) * 3} y={y} side={side} />
+        </>
+      );
+    } else {
+      return (
+        <>
+          <Square x={x} y={y} side={side} />
+          <Square x={x} y={y + side + border} side={side} />
+          <Square x={x} y={y + 2 * (side + border)} side={side} />
+          <Square x={x} y={y + 3 * (side + border)} side={side} />
+        </>
+      );
+    }
   }
 }
---
+++ app/components/figures/L.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Square } from './Square';
+
+export class O extends React.Component {
+  render() {
+    const side = 10;
+    const x = 30;
+    const y = 30;
+    const border = 1;
+
+    return (
+      <>
+        
+      </>
+
+    );
+  }
+}
