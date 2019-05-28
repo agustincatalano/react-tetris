3d58f73e7c4f80dd6f40cb306579d486c6ec7b63
agustin <agustincatalano@gmail.com>
2019-05-28 21:48:39 +0200

    Create random position

--- app/components/figures/L.js
+++ app/components/figures/L.js
@@ -7,40 +7,48 @@ export class L extends React.Component {
     const x = 200;
     const y = 30;
     const border = 1;
+    const { position } = this.props
 
-    return (
-      // L position one
-      // <>
-      //   <Square x={x} y={y} side={side} />
-      //   <Square x={x} y={y + side + border} side={side} />
-      //   <Square x={x} y={y + 2 * (side + border)} side={side} />
-      //   <Square x={x + side + border} y={y + 2 * (side + border)} side={side} />
-      // </>
+    if (position === 1) {
+      return (
+        <>
+          <Square x={x} y={y} side={side} />
+          <Square x={x} y={y + side + border} side={side} />
+          <Square x={x} y={y + 2 * (side + border)} side={side} />
+          <Square x={x + side + border} y={y + 2 * (side + border)} side={side} />
+        </>
+      )
+    } if (position === 2) {
+      return (
+        <>
+          <Square x={x + side + border} y={y} side={side} />
+          <Square x={x + side + border} y={y + side + border} side={side} />
+          <Square x={x + side + border} y={y + 2 * (side + border)} side={side} />
+          <Square x={x} y={y + 2 * (side + border)} side={side} />
+        </>
+      )
+    } if (position === 3) {
+      return (
+        <>
+          <Square x={x + 2 * side + border} y={y} side={side} />
+          <Square x={x + side + border} y={y} side={side} />
+          <Square x={x + side + border} y={y + side + border} side={side} />
+          <Square x={x + side + border} y={y + 2 * (side + border)} side={side} />
+        </>
+      )
+    }
 
-      // position 2
-      // <>
-      //   <Square x={x+side+border} y={y} side={side} />
-      //   <Square x={x+side+border} y={y + side + border} side={side} />
-      //   <Square x={x+side+border} y={y + 2 * (side + border)} side={side} />
-      //   <Square x={x} y={y + 2 * (side + border)} side={side} />
-      // </>
+    else if (position === 4) {
+      return (
+        <>
+          <Square x={x + (2 * side) + border} y={y} side={side} />
+          <Square x={x + side + border} y={y} side={side} />
+          <Square x={x + 2 * side + border} y={y + side + border} side={side} />
+          <Square x={x + 2 * side + border} y={y + 2 * (side + border)} side={side} />
+        </>
+      )
+    }
 
-      // // position 3
-      <>
-        <Square x={x + 2 * side + border} y={y} side={side} />
-        <Square x={x + side + border} y={y} side={side} />
-        <Square x={x + side + border} y={y + side + border} side={side} />
-        <Square x={x + side + border} y={y + 2 * (side + border)} side={side} />
-      </>
-
-      // position 4
-      // <>
-      //   <Square x={x + (2*side) + border} y={y} side={side} />
-      //   <Square x={x+side+border} y={y} side={side} />
-      //   <Square x={x+2*side+border} y={y + side + border} side={side} />
-      //   <Square x={x+2*side+border} y={y + 2 * (side + border)} side={side} />
-
-      // </>
-    );
   }
 }
+
