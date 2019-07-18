122f1dd0296829970605885373d8ab6c06f86648
acatalano <agustin.catalano@williamhill.com>
2019-07-18 12:30:36 +0200

    Added proper rotations and J class

--- app/components/figures/J.js
+++ app/components/figures/J.js
@@ -1,70 +1,63 @@
 import React from 'react';
 import { Square } from './Square';
 
-export class J extends React.Component {
-  render(){
-    const side = 10;
-    const x = 250;
-    const y = 80;
-    const border = 1;
-    const { position } = this.props;
+export class J extends React.Component{
+    render(){
+        const side = 10;
+        const x = 250;
+        const y = 80;
+        const border = 1;
+        const { position } = this.props;
+        
+            if (position === 0) {
+              return (
+                <>
+                  <Square x={x} y={y} side={side} />
+                  <Square x={x} y={y + side + border} side={side} />
+                  <Square x={x} y={y + 2 * (side + border)} side={side} />
+                  <Square x={x - side - border} y={y + 2 * (side + border)} side={side}
+                  />
+                </>
+              );
+            }
+            if (position === 1) {
+              return (
+        
+                <>
+                <Square x={x} y={y} side={side} />
+                <Square x={x} y={y - side - border} side={side} />
+                <Square x={x+side+border} y={y} side={side} />
+                <Square x={x+(side*2)+(border*2)} y={y} side={side} />
+              </>
+              );
+            }
+            if (position === 2) {
+              return (
+                <>
+                <Square x={x + 2 * side + border} y={y} side={side} />
+                <Square x={x + (side*3) + (border*2)} y={y} side={side} />
+                <Square x={x + 2 * side + border} y={y + side + border} side={side} />
+                <Square
+                  x={x + 2 * side + border}
+                  y={y + 2 * (side + border)}
+                  side={side}
+                />
+              </>
+              );
+            }
+        
+            if (position === 3) {
+              return (
+                <>
+                <Square x={x} y={y + side + border} side={side} />
+                <Square x={x + (side *2) + (border*2)} y={(y+ side*2) + (border *2)} side={side} />
+                <Square x={x+side+border} y={y + side + border } side={side} />
+                <Square x={x+(side*2)+(border*2)} y={y+ side + border} side={side} />
+              </>
+              );
+            }
+            return null;
+          }
 
-    if (position === 0) {
-      return (
-        <>
-          <Square x={x} y={y} side={side} />
-          <Square x={x} y={y + side + border} side={side} />
-          <Square x={x} y={y + 2 * (side + border)} side={side} />
-          <Square
-            x={x - side - border}
-            y={y + 2 * (side + border)}
-            side={side}
-          />
-        </>
-      );
-    }
-    if (position === 1) {
-      return (
-        <>
-          <Square x={x} y={y} side={side} />
-          <Square x={x} y={y - side - border} side={side} />
-          <Square x={x + side + border} y={y} side={side} />
-          <Square x={x + side * 2 + border * 2} y={y} side={side} />
-        </>
-      );
-    }
-    if (position === 2) {
-      return (
-        <>
-          <Square x={x + 2 * side + border} y={y} side={side} />
-          <Square x={x + side * 3 + border * 2} y={y} side={side} />
-          <Square x={x + 2 * side + border} y={y + side + border} side={side} />
-          <Square
-            x={x + 2 * side + border}
-            y={y + 2 * (side + border)}
-            side={side}
-          />
-        </>
-      );
-    }
 
-    if (position === 3) {
-      return (
-        <>
-          <Square x={x} y={y + side + border} side={side} />
-          <Square
-            x={x + side * 2 + border * 2}
-            y={y + side * 2 + border * 2}
-            side={side}
-          />
-          <Square x={x + side + border} y={y + side + border} side={side} />
-          <Square
-            x={x + side * 2 + border * 2}
-            y={y + side + border}
-            side={side}
-          />
-      );
     }
-    return null;
-  }
-}
